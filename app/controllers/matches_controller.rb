5491a6247b66fa6be5d219acbdbbe12ac498ed13
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2011-12-21 15:29:52 -0500

    Create views for matches. Move towards using new Game model.

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,22 @@
 class MatchesController < ApplicationController
+  def index
+    @matches = Match.all
+  end
+  
+  def show
+    @match = Match.find(params[:id])
+  end
+
+  def new
+    @match = Match.new
+  end
+  
+  def create
+    @match = Match.new(params[:match])
+    if @match.save!
+      redirect_to @match
+    else
+      render :action => 'new'
+    end
+  end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,27 @@
 class Match < ActiveRecord::Base
+  attr_accessor :winner_names, :loser_names, :game_name
+  
   has_many :participants, :inverse_of => :match
-  has_many :players, :through => :participants, :inverse_of => :matchs
+  has_many :players, :through => :participants, :inverse_of => :matches
+  validate :game, :presence => true
+  
+  def winners
+    Player.joins(:participants).where(:participants => {:winner => true, :match_id => self.id})
+  end
+  def losers
+    Player.joins(:participants).where(:participants => {:winner => false, :match_id => self.id})
+  end
+  before_save do
+    game_name.downcase!
+    game = Game.find_or_create_by_name(game_name)
+  end
+  after_create do
+    winners_text.split.map(&:downcase).each do |winner|
+      Participant.create(:player => Player.find_or_create_by_name(winner), :match => self, :winner => true)
+    end
+    losers_text.split.map(&:downcase).each do |loser|
+      Participant.create(:player => Player.find_or_create_by_name(loser), :match => self, :winner => false)
+    end
+  end
+  
 end
