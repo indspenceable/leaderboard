c03e8d8e3f61951895ec8c5c23fbef3e0d547ed2
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2011-12-21 15:24:50 -0500

    Fixed join in wins() and losses()

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,11 +3,9 @@ class Player < ActiveRecord::Base
   has_many :matches, :through => :participants, :inverse_of => :players
   
   def wins
-    matches.joins(:participants)
-    .where(:participants => {:winner => true})
+    Match.joins(:participants).where(:participants => {:winner => true, :player_id => self.id})
   end
   def losses
-    matches.joins(:participants)
-    .where(:participants => {:winner => false})
+    Match.joins(:participants).where(:participants => {:winner => false, :player_id => self.id})
   end
 end
